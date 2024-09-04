6b7c29301a91782e9255a31f9e29b493b090fc09
Nikhil Gohil <40459568+NikhilGohil@users.noreply.github.com>
2024-09-04 17:41:59 +0530

    Final Commit

--- lib/Pages/searchPage.dart
+++ lib/Pages/searchPage.dart
@@ -15,24 +15,25 @@ class _SearchpageState extends State<Searchpage> {
   TextEditingController _searchController = TextEditingController();
   List<String> suggestions = [];
   OutlineInputBorder borderStyle = OutlineInputBorder(
-    borderRadius: BorderRadius.circular(5),
-    borderSide: const BorderSide(color: Colors.grey, width: 1)
-  );
+      borderRadius: BorderRadius.circular(5),
+      borderSide: const BorderSide(color: Colors.grey, width: 1));
   @override
   void initState() {
     getSuggestions();
     super.initState();
   }
+
   void getSuggestions() async {
     SharedPreferences _sp = await SharedPreferences.getInstance();
     suggestions = (await _sp.getStringList('suggestions'))!;
     setState(() {});
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: Appbarcomp(),
+        backgroundColor: Colors.white,
+        appBar: Appbarcomp(),
         body: Column(
           children: [
             Padding(
@@ -40,31 +41,37 @@ class _SearchpageState extends State<Searchpage> {
               child: Container(
                 decoration: BoxDecoration(boxShadow: [
                   BoxShadow(
-                    color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.1),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: const Offset(0, 0)
-                  )
+                      color:
+                          const Color.fromARGB(255, 0, 0, 0).withOpacity(0.1),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 0))
                 ]),
                 child: TextField(
                   controller: _searchController,
                   cursorColor: Colors.grey,
                   decoration: InputDecoration(
-                    hintText: 'Search...',
-                    fillColor: Colors.white,
-                    filled: true,
-                    enabledBorder: borderStyle,
-                    focusedBorder: borderStyle,
-                    suffixIcon: IconButton(onPressed: () async {
-                      SharedPreferences _sp = await SharedPreferences.getInstance();
-                      if(suggestions.length >= 4) {
-                        suggestions.removeLast();
-                      }
-                      suggestions.insert(0, _searchController.text);
-                      await _sp.setStringList('suggestions', suggestions);
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => Bookssearchviewcomp(result: _searchController.text)));
-                    }, icon: Icon(Icons.search))
-                  ),
+                      hintText: 'Search...',
+                      fillColor: Colors.white,
+                      filled: true,
+                      enabledBorder: borderStyle,
+                      focusedBorder: borderStyle,
+                      suffixIcon: IconButton(
+                          onPressed: () async {
+                            SharedPreferences _sp =
+                                await SharedPreferences.getInstance();
+                            if (suggestions.length >= 4) {
+                              suggestions.removeLast();
+                            }
+                            suggestions.insert(0, _searchController.text);
+                            await _sp.setStringList('suggestions', suggestions);
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Bookssearchviewcomp(
+                                        result: _searchController.text)));
+                          },
+                          icon: Icon(Icons.search))),
                 ),
               ),
             ),
@@ -73,18 +80,28 @@ class _SearchpageState extends State<Searchpage> {
               shrinkWrap: true,
               physics: NeverScrollableScrollPhysics(),
               itemBuilder: (context, index) => Container(
-                decoration: BoxDecoration(boxShadow: [BoxShadow(color: const Color.fromARGB(145, 158, 158, 158),spreadRadius: 1, blurRadius: 1)],color: Colors.grey[200],borderRadius: BorderRadius.circular(10)),
+                decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: const Color.fromARGB(145, 158, 158, 158),
+                          spreadRadius: 1,
+                          blurRadius: 1)
+                    ],
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10)),
                 child: ListTile(
                   splashColor: brown.withOpacity(0.5),
                   tileColor: Colors.white,
                   onLongPress: () async {
-                    SharedPreferences _sp = await SharedPreferences.getInstance();
+                    SharedPreferences _sp =
+                        await SharedPreferences.getInstance();
                     suggestions.removeAt(index);
                     await _sp.setStringList('suggestions', suggestions);
                     setState(() {});
                   },
                   onTap: () {
-                    _searchController.value = TextEditingValue(text: suggestions[index]);
+                    _searchController.value =
+                        TextEditingValue(text: suggestions[index]);
                   },
                   title: Text(suggestions[index]),
                   trailing: Icon(Icons.arrow_outward),
@@ -93,7 +110,6 @@ class _SearchpageState extends State<Searchpage> {
               itemCount: suggestions?.length,
             )
           ],
-        )
-    );
+        ));
   }
 }
